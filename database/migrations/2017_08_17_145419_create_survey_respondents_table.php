2a810799ecbb577b538d0fc9c54694298e498c1d
mwakaambrose <ambrosemwaka@gmail.com>
2017-11-13 20:03:06 +0300

    Makes phone field to string to accomodate the (+) prefix

--- database/migrations/2017_08_17_145419_create_survey_respondents_table.php
+++ database/migrations/2017_08_17_145419_create_survey_respondents_table.php
@@ -21,7 +21,7 @@ class CreateSurveyRespondentsTable extends Migration
             $table->integer('age')->nullable();
             $table->string('image_url')->nullable();
             $table->string('occupation')->nullable();
-            $table->integer('phone_number')->nullable();
+            $table->string('phone_number')->nullable();
             $table->string('facebook_id');
             $table->string('email')->nullable();
             $table->string('city')->nullable();
