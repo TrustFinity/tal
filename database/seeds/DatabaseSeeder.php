462f15745c799ffd4bdbba6162b7e1ab83eeea8c
mwakaambrose <ambrosemwaka@gmail.com>
2018-02-27 19:34:33 +0300

    disables the other seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
         // DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
         $this->call(UserSeeder::class);
-        $this->call(SurveySeeder::class);
-        $this->call(QuestionSeeder::class);
-        $this->call(ResponseSeeder::class);
-        $this->call(RespondentSeeder::class);
-        $this->call(RespondentResponseSeeder::class);
+        // $this->call(SurveySeeder::class);
+        // $this->call(QuestionSeeder::class);
+        // $this->call(ResponseSeeder::class);
+        // $this->call(RespondentSeeder::class);
+        // $this->call(RespondentResponseSeeder::class);
 
         // DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
